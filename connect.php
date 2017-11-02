cb7f9d30b40a1443614073475d169c10a519d1d1
Unknown <anttonlas601@gmail.com>
2017-11-02 09:38:57 +0200

    index

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	try
 	{
 	 $conn_string = "mysql:host=localhost;dbname=har1";
-	 $db = new PDO ($conn_string, "my_user", "my_pass");
+	 $db = new PDO ($conn_string, "webuser", "webpass");
 	 $db->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	 print ("Connected\n");
 	}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include "menu.php"; ?>
+<h1>PHP-esimerkki</h1>
+<p>
+  Tämä on ensimmäinen esimerkki
+</p>
+<?php include "footer.php"; ?>
